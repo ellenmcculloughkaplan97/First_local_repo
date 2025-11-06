9f2faa9b2d15cdb3cbd52f6450daaf5f3c797cf9
Ellen McCullough <ellen.mccullough@kaplan.com>
2025-11-06 11:45:03 +0000

    Create test_spyder_file.py

---
+++ test_spyder_file.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  6 11:44:01 2025
+
+@author: ellen
+"""
+
+'spyder file I want to keep track of'
+
+
+
+def say_hi():
+    return 'hi there, this is a greeting'
